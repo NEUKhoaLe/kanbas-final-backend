8c62f0db66a7903d715078548c6610d2d119a922
Rixuan Zheng <zheng.rix@northeastern.edu>
2024-04-18 21:02:43 -0400

    stop courses infinite load

--- kanbas/courses/dao.js
+++ kanbas/courses/dao.js
@@ -1,7 +1,8 @@
 import model from "./model.js";
-export const createCourse = (course) => {
-  delete course._id;
-  model.create(course);
+import { Types } from "mongoose";
+export const createCourse = async (course) => {
+  course = { ...course, _id: new Types.ObjectId() };
+  return await model.create(course);
 };
 export const findAllCourses = () => model.find();
 export const updateCourse = (courseId, course) => model.updateOne({ _id: courseId }, { $set: course });
